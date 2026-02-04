f7176d6f2ff5973a6382dbb17dccfe16f7649b13
김병곤 <fharenheit@gmail.com>
2026-02-04 13:59:54 +0900

    Add Json

---
+++ postgresql-odbc-devart/Device.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace postgresql_odbc_devart;
+
+internal class Device
+{
+    [JsonProperty("device_id")] // JSON의 device_id를 DeviceId에 매핑
+    public string DeviceId { get; set; }
+
+    [JsonIgnore] // 이 속성은 JSON 결과에 포함하지 않음
+    public string InternalCode { get; set; }
+}
---
+++ postgresql-odbc-devart/Json.cs
@@ -0,0 +1,24 @@
+﻿using log4net;
+using Newtonsoft.Json.Linq;
+using Npgsql;
+
+namespace YourNamespace;
+
+internal class Json
+{
+    private static readonly ILog log = LogManager.GetLogger(typeof(Json));
+    
+    private static async Task Main(string[] args)
+    {
+        log.Info("Json 직렬화 시작");
+
+        string json = "{ 'price': 1500, 'items': ['apple', 'banana'] }";
+        JObject jo = JObject.Parse(json);
+
+        // 특정 필드만 추출
+        int price = (int)jo["price"];
+        string firstItem = (string)jo["items"][0];
+
+        log.Info($"가격: {price}, 첫 번째 아이템: {firstItem}");
+    }
+}
